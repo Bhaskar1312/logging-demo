7b3e5680e881af1b0d532b4b0c3506de0a354e78
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2023-11-06 00:32:38 +0530

    print logback internal status in case of no error during build

--- src/main/java/com/example/loggingdemo/LoggingDemoApplication.java
+++ src/main/java/com/example/loggingdemo/LoggingDemoApplication.java
@@ -1,5 +1,9 @@
 package com.example.loggingdemo;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LoggingDemoApplication {
 
     public static void main(String[] args) {
+        // if no error, add two lines of code
+        // assume SLF4J is bound to logback in the current environment
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        // print logback's internal status
+        StatusPrinter.print(lc);
         SpringApplication.run(LoggingDemoApplication.class, args);
     }
 
